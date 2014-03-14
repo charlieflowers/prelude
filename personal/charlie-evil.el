a022d52b8738a2e57d6a1b10103a648cedd7d483
charlieflowers <cflowers@gmail.com>
2014-03-14 14:14:28 -0400

    OH YEAH. Added EVIL LITTLE WORD

--- personal/charlie-evil.el
+++ personal/charlie-evil.el
@@ -5,3 +5,16 @@
 
 (require 'evil)
 (evil-mode 1)
+
+;; This beautiful stuff right here is from metasandwich.com. Deals with dashes and camel humps!
+(evil-define-motion evil-little-word (count)
+  :type exclusive
+  (let* ((case-fold-search nil)
+         (count (if count count 1)))
+    (while (> count 0)
+      (forward-char)
+      (search-forward-regexp "[_A-Z]\\|\\W" nil t)
+      (backward-char)
+      (decf count))))
+
+(define-key evil-operator-state-map (kbd "lw") 'evil-little-word)
