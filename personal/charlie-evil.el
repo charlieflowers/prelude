8e469c4b4e14d6f4ae4b6257592c666dfa3716ce
charlieflowers <cflowers@gmail.com>
2014-03-14 15:18:00 -0400

    got my g1 and gb keys working again, added coloring of the state indicator

--- personal/charlie-evil.el
+++ personal/charlie-evil.el
@@ -18,3 +18,31 @@
       (decf count))))
 
 (define-key evil-operator-state-map (kbd "lw") 'evil-little-word)
+
+;; This is from cofi's evil config ... it colors the "evil state" indicator, to make it stand out more.
+(setq evil-normal-state-tag   (propertize "N" 'face '((:background "green" :foreground "black")))
+      evil-emacs-state-tag    (propertize "E" 'face '((:background "orange" :foreground "black")))
+      evil-insert-state-tag   (propertize "I" 'face '((:background "red")))
+      evil-motion-state-tag   (propertize "M" 'face '((:background "blue")))
+      evil-visual-state-tag   (propertize "V" 'face '((:background "grey80" :foreground "black")))
+      evil-operator-state-tag (propertize "O" 'face '((:background "purple"))))
+
+;; Certain modes need to start up in Emacs mode (especially the freaking repl! ... or does it?) And this makes that happen (taken from cofi evil)
+(loop for (mode . state) in '((inferior-emacs-lisp-mode      . emacs)
+                              (pylookup-mode                 . emacs)
+                              (comint-mode                   . emacs)
+                              (ebib-entry-mode               . emacs)
+                              (ebib-index-mode               . emacs)
+                              (ebib-log-mode                 . emacs)
+                              (gtags-select-mode             . emacs)
+                              (shell-mode                    . emacs)
+                              (term-mode                     . emacs)
+                              (bc-menu-mode                  . emacs)
+                              (magit-branch-manager-mode-map . emacs)
+                              (semantic-symref-results-mode  . emacs)
+                              (rdictcc-buffer-mode           . emacs))
+      do (evil-set-initial-state mode state))
+
+;; These are key combos I have chosen personally.
+(define-key evil-normal-state-map "g1" 'delete-other-windows) ;; Like emacs C-x 1 (I hope)
+(define-key evil-normal-state-map "gb" 'ido-switch-buffer)    ;; Hopefully same as C-x b
